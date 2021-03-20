7ac2be70375fa7fbf1a680de7947d8efa4c60df1
John Glick <glick@sandiego.edu>
2021-03-20 14:41:25 -0700

    Initial commit

---
+++ longest_increasing_substring.py
@@ -0,0 +1,18 @@
+"""
+File: longest_increasing_substring.py 
+Author: COMP 120 class
+Date: March 23, 2021
+Description: Has function for longest_increasing_substring.
+"""
+def longest_increasing_substring(s):
+    """
+    Returns the longest substring in s.  In case of
+    ties, returns the first longest substring.
+    """
+    pass
+
+if __name__ == "__main__":
+    s = input("Enter a string: ")
+    print(f"Maximum consecutive increasingly ordered substring is {longest_increasing_substring(s)}")
+
+
